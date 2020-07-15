c648e9ccfa22cd0fe8c86ae29120f920825df0f3
Frank Lin <xiahong+frankvee@bodhi.network>
2020-07-15 12:23:57 -0700

    Update Unioracle router address.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { ChainId, JSBI, Percent, Token, WETH } from '@uniswap/sdk'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0xF7Ac2090a53Cb7aCD3Dcab93f632C550fD309d72'
 
 // a list of tokens by chain
 type ChainTokenList = {
